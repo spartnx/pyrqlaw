c05ec7b3e8928a24309f20ae6ba32ee6ee76022f
spartnx <tristan-sarton@hotmail.fr>
2023-05-02 14:56:51 -0400

    Remove unneeded import in __init__.py

--- pyrqlaw/__init__.py
+++ pyrqlaw/__init__.py
@@ -2,29 +2,5 @@
 init file for module
 """
 
-
-from ._symbolic import symbolic_rqlaw_mee_with_a
-
-from ._integrate import (
-    eom_mee_with_a_gauss,
-    rk4, 
-    rkf45,
-)
-from ._lyapunov import lyapunov_control_angles
-from ._elements import (
-    sv2kep, 
-    kep2sv,
-    kep2mee,
-    mee2kep,
-    kep2mee_with_a,
-    mee_with_a2sv,
-)
 from ._rqlaw import RQLaw
-from ._plot_helper import (
-    get_sphere_coordinates,
-    plot_sphere_wireframe,
-    get_ellipsoid_coordinates,
-    plot_ellipsoid_wireframe,
-    set_equal_axis,
-)
 from ._tuner import RQLawTuner
